803654afca335fe804c9111e40bc1a213c847944
Erika Wu <ironance@hotmail.com>
2019-04-08 13:31:53 +0800

    modify reply

--- app.py
+++ app.py
@@ -36,9 +36,17 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = 'I do not know what you said.'
+
+    if msg == 'hi':
+        r = 'hi'
+    elif msg == 'eat?':
+        r = 'No, thanks.'
+        
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
